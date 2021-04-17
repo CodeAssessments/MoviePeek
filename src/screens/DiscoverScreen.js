ee13933f4704e7ca14aeb00da59d037819877bba
wizdo-johan <soldierheart@hotmail.com>
2021-04-17 15:57:13 +0200

    tmdb API

--- src/screens/DiscoverScreen.js
+++ src/screens/DiscoverScreen.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import { Text, TouchableOpacity, View, FlatList } from 'react-native'
+import axios from 'axios'
+
+const API_KEY = process.env.TMDB_API_KEY;
 
 const DiscoverScreen = ({navigation}) => {
+    const [popularMovies, setPopularMovies] = useState([]);
+    useEffect(async () => {
+        const list = await getMovieList();
+        setPopularMovies(list)
+    }, [])
+
+    const getMovieList = () => {
+        return axios.get('https://api.themoviedb.org/3/movie/popular?api_key='+API_KEY)
+        .then(res => res.data.results)
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+
     return (
         <View>
             <TouchableOpacity onPress={() => {
                 navigation.navigate('Detail')
             }}>
                 <Text>Detail</Text>
+                <FlatList 
+                data={popularMovies}
+                extraData={popularMovies}
+                renderItem={({item}) => <Text>{item.title}</Text>}
+                keyExtractor={item => item.id.toString()}
+            />
             </TouchableOpacity>
         </View>
     )
